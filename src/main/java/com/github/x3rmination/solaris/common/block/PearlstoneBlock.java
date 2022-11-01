1761351e49a3826535b2fdf2e31bf381f31734f8
X3RMINATION <nicolaeungur@gmail.com>
2022-11-01 16:57:28 -0700

    test for discord

--- src/main/java/com/github/x3rmination/solaris/common/block/PearlstoneBlock.java
+++ src/main/java/com/github/x3rmination/solaris/common/block/PearlstoneBlock.java
@@ -27,6 +27,7 @@ public class PearlstoneBlock extends Block {
         if(pLevel.getBrightness(LightLayer.BLOCK, pPos.above()) > 5 || pLevel.getBrightness(LightLayer.SKY, pPos.above()) > 5) {
             pLevel.sendParticles(ParticleRegistry.ANIMATED_SPARKS.get(), pPos.getX() + 0.5D, pPos.getY(), pPos.getZ() + 0.5D, 10, 0, 0, 0, 0.4D);
         }
+        //
     }
 
 }
