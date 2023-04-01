1fca62d6a46b07afe9a3f23a843d18df8bc5b560
X3RMINATION <nicolaeungur@gmail.com>
2023-04-01 10:35:51 -0700

    Fixed Spear offset

--- src/main/java/com/github/x3rmination/solaris/common/item/PhoenixSpear/PhoenixSpearRenderer.java
+++ src/main/java/com/github/x3rmination/solaris/common/item/PhoenixSpear/PhoenixSpearRenderer.java
@@ -1,10 +1,23 @@
 package com.github.x3rmination.solaris.common.item.PhoenixSpear;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.world.item.ItemStack;
 import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
+import yesman.epicfight.client.ClientEngine;
 
 public class PhoenixSpearRenderer extends GeoItemRenderer<PhoenixSpearItem> {
 
     public PhoenixSpearRenderer() {
         super(new PhoenixSpearModel());
+    }
+
+    @Override
+    public void renderByItem(ItemStack stack, ItemTransforms.TransformType transformType, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
+        if(ClientEngine.instance.controllEngine.getPlayerPatch().isBattleMode() && (transformType.equals(ItemTransforms.TransformType.THIRD_PERSON_LEFT_HAND) || transformType.equals(ItemTransforms.TransformType.THIRD_PERSON_RIGHT_HAND))) {
+            poseStack.translate(0, 0.3, -0.125);
+        }
+        super.renderByItem(stack, transformType, poseStack, bufferSource, packedLight, packedOverlay);
     }
 }
