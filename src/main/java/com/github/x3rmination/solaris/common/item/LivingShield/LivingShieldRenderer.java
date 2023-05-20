0e3e7a03104610096632e350fcb30bcd09391951
X3RMINATION <nicolaeungur@gmail.com>
2023-05-19 20:38:53 -0700

    Fixed shield offset

--- src/main/java/com/github/x3rmination/solaris/common/item/LivingShield/LivingShieldRenderer.java
+++ src/main/java/com/github/x3rmination/solaris/common/item/LivingShield/LivingShieldRenderer.java
@@ -1,9 +1,22 @@
 package com.github.x3rmination.solaris.common.item.LivingShield;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.world.item.ItemStack;
 import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
+import yesman.epicfight.client.ClientEngine;
 
 public class LivingShieldRenderer extends GeoItemRenderer<LivingShieldItem> {
     public LivingShieldRenderer() {
         super(new LivingShieldModel());
+    }
+
+    @Override
+    public void renderByItem(ItemStack stack, ItemTransforms.TransformType transformType, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
+        boolean left = transformType == ItemTransforms.TransformType.THIRD_PERSON_LEFT_HAND || transformType == ItemTransforms.TransformType.FIRST_PERSON_LEFT_HAND;
+        if(left && ClientEngine.instance.controllEngine.getPlayerPatch().isBattleMode()) {
+            poseStack.translate(1, 0, 0);
+        }
     }
 }
