4c5ad563aba64435e30eadb9f5bf3da6f463cf15
X3RMINATION <nicolaeungur@gmail.com>
2023-05-24 16:10:14 -0700

    made blizzard particle denser and larger

--- src/main/java/com/github/x3rmination/solaris/client/particle/BlizzardParticle.java
+++ src/main/java/com/github/x3rmination/solaris/client/particle/BlizzardParticle.java
@@ -14,7 +14,7 @@ public class BlizzardParticle extends TextureSheetParticle {
     public BlizzardParticle(ClientLevel pLevel, double pX, double pY, double pZ) {
         super(pLevel, pX, pY, pZ);
         this.quadSize *= 0.75F;
-        this.lifetime = 100;
+        this.lifetime = 200;
         this.hasPhysics = false;
         this.friction = 1;
     }
@@ -23,10 +23,10 @@ public class BlizzardParticle extends TextureSheetParticle {
     public void tick() {
         this.onGround = false;
         super.tick();
-        double r = 2.5*Math.log(age);
-        this.xd = r*(0.075)*(Math.sin(age/2.5) + Math.sin(age/2.5 + 0.01D));
-        this.zd = r*(0.075)*(Math.cos(age/2.5) + Math.cos(age/2.5 + 0.01D));
-        this.yd = 0.25*Math.sin(2*age + 34F);
+        double r = 3.5*Math.log(age);
+        this.xd = r*(0.065)*(Math.sin(age/2.5) + Math.sin(age/2.5 + 0.01D)) + ((this.random.nextFloat() - 0.5) * 0.1);
+        this.zd = r*(0.065)*(Math.cos(age/2.5) + Math.cos(age/2.5 + 0.01D)) + ((this.random.nextFloat() - 0.5) * 0.1);
+        this.yd = 0.25*Math.sin(2*age + 34F) + ((this.random.nextFloat() - 0.5) * 0.1);
     }
 
     @Override
